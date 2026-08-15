7484816bc98571079c047af8aded67471c4b3f1a
nshakib <nazmus.shakib.cse@gmail.com>
2026-08-15 14:45:09 +0600

    fix: all seven route added in app.ts file

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,12 @@ import config from "./app/config";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import { notFound } from "./app/middlewares/notFound";
 import { AuthRoutes } from "./app/module/auth/auth.route";
+import { UserRoutes } from "./app/module/user/user.route";
+import { RestaurantRoutes } from "./app/module/restaurant/restaurant.route";
+import { MenuItemRoutes } from "./app/module/menuItem/menuItem.route";
+import { OrderRoutes } from "./app/module/order/order.route";
+import { ReviewRoutes } from "./app/module/review/review.route";
+import { DashboardRoutes } from "./app/module/dashboard/dashboard.route";
 
 const app: Application = express();
 
@@ -16,24 +22,26 @@ app.use(
   }),
 );
 
-// Enable URL-encoded form data parsing
 app.use(express.urlencoded({ extended: true }));
-
-// Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", AuthRoutes);
+app.use("/api/v1/users", UserRoutes);
+app.use("/api/v1/restaurants", RestaurantRoutes);
+app.use("/api/v1/menu-items", MenuItemRoutes);
+app.use("/api/v1/orders", OrderRoutes);
+app.use("/api/v1/reviews", ReviewRoutes);
+app.use("/api/v1/dashboard", DashboardRoutes);
 
-// Basic route
 app.get("/", async (req: Request, res: Response) => {
   res.status(httpStatus.OK).json({
     success: true,
-    message: "Welcome to PH Healthcare System Backend",
+    message: "Welcome to ForkLane order.uk Backend",
   });
 });
 
-app.use(globalErrorHandler);
 app.use(notFound);
+app.use(globalErrorHandler);
 
 export default app;
